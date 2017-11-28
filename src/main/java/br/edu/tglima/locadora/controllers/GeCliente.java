ea1593f3aadb1fcc0f66ced899fee3acc1858547
tglima <thg.limadesousa@gmail.com>
2017-11-28 09:22:59 -0200

    Implementa o controller responsável por gerenciar os clientes

---
+++ src/main/java/br/edu/tglima/locadora/controllers/GeCliente.java
@@ -0,0 +1,110 @@
+package br.edu.tglima.locadora.controllers;
+
+import static br.edu.tglima.locadora.util.FacesUtil.enviarMsgErro;
+import static br.edu.tglima.locadora.util.FacesUtil.enviarMsgSucesso;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.edu.tglima.locadora.models.pessoa.Cliente;
+import br.edu.tglima.locadora.repository.ClienteRepository;
+
+@Named
+@ApplicationScoped
+public class GeCliente implements Serializable {
+
+	private static final long serialVersionUID = -3642616028842979731L;
+
+	@Inject
+	private ClienteRepository repository;
+	private List<Cliente> cliEncontrados = new ArrayList<Cliente>();
+	private Cliente selectedCli;
+	private String searchParam;
+	private boolean resultEmpty;
+	private byte tipoDeBusca = 0;
+
+	public void clearInput() {
+		searchParam = null;
+		resultEmpty = false;
+	}
+
+	public void buscarPelaCNH() {
+		cliEncontrados.clear();
+		try {
+			cliEncontrados.add(repository.buscarPorHabilitacao(searchParam));
+		} catch (Exception e) {
+			if (e.getMessage().contains("No entity found for query")) {
+				resultEmpty = true;
+			} else {
+				enviarMsgErro("Erro ao realizar a pesquisa! \n" + e.getMessage());
+			}
+		}
+
+	}
+
+	public void buscarPeloNome() {
+		cliEncontrados.clear();
+		try {
+			cliEncontrados = repository.buscaPorNome(searchParam.toLowerCase(), searchParam.toLowerCase());
+			if (cliEncontrados.isEmpty()) {
+				resultEmpty = true;
+			}
+		} catch (Exception e) {
+			enviarMsgErro("Erro ao realizar a pesquisa! " + e.getMessage());
+		}
+
+	}
+
+	public void salvar() {
+		try {
+			repository.salvarEdicao(selectedCli);
+			enviarMsgSucesso("Alterações salvas com sucesso!");
+		} catch (Exception e) {
+			enviarMsgErro("Erro, as alterações não foram salvas!");
+			if (e.getMessage().contains("ConstraintViolationException")) {
+				enviarMsgErro("O Nº da CNH informada já pertence a outra pessoa.");
+			} else {
+				enviarMsgErro(e.getMessage());
+			}
+		}
+
+	}
+
+	public byte getTipoDeBusca() {
+		return tipoDeBusca;
+	}
+
+	public void setTipoDeBusca(byte tipoDeBusca) {
+		this.tipoDeBusca = tipoDeBusca;
+	}
+
+	public String getSearchParam() {
+		return searchParam;
+	}
+
+	public void setSearchParam(String searchParam) {
+		this.searchParam = searchParam;
+	}
+
+	public boolean isResultEmpty() {
+		return resultEmpty;
+	}
+
+	public List<Cliente> getCliEncontrados() {
+		return cliEncontrados;
+	}
+
+	public Cliente getSelectedCli() {
+		return selectedCli;
+	}
+
+	public void setSelectedCli(Cliente selectedCli) {
+		this.selectedCli = selectedCli;
+	}
+
+}
