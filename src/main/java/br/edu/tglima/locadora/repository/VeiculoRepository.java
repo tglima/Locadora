5982db88cc052a4af02e7566655d9ddaed383d90
tglima <thg.limadesousa@gmail.com>
2017-11-30 09:58:47 -0200

    Atualiza o VeiculoRepository para ser compatível com GeVeic

--- src/main/java/br/edu/tglima/locadora/repository/VeiculoRepository.java
+++ src/main/java/br/edu/tglima/locadora/repository/VeiculoRepository.java
@@ -33,6 +33,37 @@ public class VeiculoRepository extends AbstractRepository<Veiculo> {
 		return query.getResultList();
 	}
 
+	public List<Veiculo> buscarPorMarca(OpMarcas marca, OpStatus status, boolean statusEqual)
+			throws Exception {
+		entityManager = JpaUtil.getEntityManager();
+		String sql;
+		if (statusEqual) {
+			sql = "SELECT v FROM Veiculo v WHERE v.marca = :marca AND v.status = :status";
+		} else {
+			sql = "SELECT v FROM Veiculo v WHERE v.marca = :marca AND v.status != :status";
+		}
+		TypedQuery<Veiculo> query = entityManager.createQuery(sql, Veiculo.class);
+		query.setParameter("marca", marca);
+		query.setParameter("status", OpStatus.ALUGADO);
+		return query.getResultList();
+	}
+
+	public List<Veiculo> buscaPorCategoria(OpCategorias cat, OpStatus status, boolean statusEqual)
+			throws Exception {
+		entityManager = JpaUtil.getEntityManager();
+		String sql;
+		if (statusEqual) {
+			sql = "SELECT v FROM Veiculo v WHERE v.categoria = :categoria AND v.status = :status";
+		} else {
+			sql = "SELECT v FROM Veiculo v WHERE v.categoria = :categoria AND v.status != :status";
+		}
+
+		TypedQuery<Veiculo> query = entityManager.createQuery(sql, Veiculo.class);
+		query.setParameter("categoria", cat);
+		query.setParameter("status", status);
+		return query.getResultList();
+	}
+
 	public List<Veiculo> buscaPorParametros(OpStatus status, OpMarcas marca) throws Exception {
 		entityManager = JpaUtil.getEntityManager();
 		String sql = "SELECT v FROM Veiculo	v WHERE v.status = :status AND v.marca = :marca";
