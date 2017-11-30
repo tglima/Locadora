ccbc54058ffd29ebe9dc493ebc7af98663583fec
tglima <thg.limadesousa@gmail.com>
2017-11-30 20:34:37 -0200

    Refatora a classe CadCliente.

--- src/main/java/br/edu/tglima/locadora/controllers/CadCliente.java
+++ src/main/java/br/edu/tglima/locadora/controllers/CadCliente.java
@@ -1,6 +1,7 @@
 package br.edu.tglima.locadora.controllers;
 
 import static br.edu.tglima.locadora.util.FacesUtil.enviarMsgErro;
+import static br.edu.tglima.locadora.util.FacesUtil.enviarMsgSucesso;
 import static br.edu.tglima.locadora.util.Util.fmtToSave;
 
 import java.io.Serializable;
@@ -12,8 +13,6 @@ import javax.inject.Named;
 import br.edu.tglima.locadora.models.pessoa.Cliente;
 import br.edu.tglima.locadora.models.pessoa.OpGeneros;
 import br.edu.tglima.locadora.repository.ClienteRepository;
-import br.edu.tglima.locadora.util.FacesUtil;
-import br.edu.tglima.locadora.validators.ClienteValidator;
 
 @Named
 @RequestScoped
@@ -24,46 +23,24 @@ public class CadCliente implements Serializable {
 	private Cliente novoCliente;
 
 	@Inject
-	private ClienteValidator validator;
-
-	@Inject
 	private ClienteRepository repository;
 
 	public void cadastrar() {
-
-		if (validarCadastro()) {
-			this.novoCliente = fmtToSave(novoCliente);
-			try {
-				repository.salvarNovo(this.novoCliente);
-				FacesUtil.enviarMsgSucesso("Cliente cadastrado com sucesso!");
-				this.novoCliente = null;
-			} catch (Exception e) {
-				enviarMsgErro("Erro, cliente não cadastrado!");
-				if (e.getMessage().contains("ConstraintViolationException")) {
-					enviarMsgErro("O Nº de Registro da CNH informada já pertence a outra pessoa.");
-				} else {
-					enviarMsgErro(e.getMessage());
-				}
-
-			}
-
-		}
-
-	}
-
-	private boolean validarCadastro() {
-		boolean cadValido = false;
-		if (validator.idadeParaDirigir(this.novoCliente.getDataNasc())) {
-			if (validator.cnhNaValidade(this.novoCliente.getValidadeHab())) {
-				cadValido = true;
+		novoCliente = fmtToSave(novoCliente);
+		try {
+			repository.salvarNovo(this.novoCliente);
+			enviarMsgSucesso("Cliente cadastrado com sucesso!");
+			novoCliente = null;
+		} catch (Exception e) {
+			enviarMsgErro("Erro, cliente não cadastrado!");
+			if (e.getMessage().contains("ConstraintViolationException")) {
+				enviarMsgErro("O Nº de Registro da CNH informada já pertence a outra pessoa.");
 			} else {
-				enviarMsgErro("Habilitação vencida!");
+				enviarMsgErro(e.getMessage());
 			}
-		} else {
-			enviarMsgErro("A idade do cliente é inválida! Só serão aceitas idades entre 23 e 70 anos.");
+
 		}
 
-		return cadValido;
 	}
 
 	// Getters de acesso aos Enums
