f1f5c7744b8fb5d7604de066c821a84b1634a979
tglima <thg.limadesousa@gmail.com>
2017-12-02 20:51:06 -0200

    Atualiza VeiculoRepository para suportar o EntityManager injetado

--- src/main/java/br/edu/tglima/locadora/repository/VeiculoRepository.java
+++ src/main/java/br/edu/tglima/locadora/repository/VeiculoRepository.java
@@ -2,23 +2,25 @@ package br.edu.tglima.locadora.repository;
 
 import java.util.List;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
 import br.edu.tglima.locadora.models.veiculo.OpCategorias;
 import br.edu.tglima.locadora.models.veiculo.OpMarcas;
 import br.edu.tglima.locadora.models.veiculo.OpStatus;
 import br.edu.tglima.locadora.models.veiculo.Veiculo;
-import br.edu.tglima.locadora.util.JpaUtil;
 
 public class VeiculoRepository extends AbstractRepository<Veiculo> {
 
 	public VeiculoRepository() {
 		super(Veiculo.class);
-		entityManager = JpaUtil.getEntityManager();
 	}
 
+	@Inject
+	private EntityManager entityManager;
+
 	public Veiculo buscarPorPlaca(String placa) throws Exception {
-		entityManager = JpaUtil.getEntityManager();
 		String sql = "SELECT v FROM Veiculo v WHERE v.placa = :placa";
 		TypedQuery<Veiculo> query = entityManager.createQuery(sql, Veiculo.class);
 		query.setParameter("placa", placa);
@@ -26,7 +28,6 @@ public class VeiculoRepository extends AbstractRepository<Veiculo> {
 	}
 
 	public List<Veiculo> buscaPorStatus(OpStatus status) throws Exception {
-		entityManager = JpaUtil.getEntityManager();
 		String sql = "SELECT v FROM Veiculo v WHERE v.status = :status";
 		TypedQuery<Veiculo> query = entityManager.createQuery(sql, Veiculo.class);
 		query.setParameter("status", status);
@@ -35,7 +36,6 @@ public class VeiculoRepository extends AbstractRepository<Veiculo> {
 
 	public List<Veiculo> buscarPorMarca(OpMarcas marca, OpStatus status, boolean statusEqual)
 			throws Exception {
-		entityManager = JpaUtil.getEntityManager();
 		String sql;
 		if (statusEqual) {
 			sql = "SELECT v FROM Veiculo v WHERE v.marca = :marca AND v.status = :status";
@@ -50,7 +50,6 @@ public class VeiculoRepository extends AbstractRepository<Veiculo> {
 
 	public List<Veiculo> buscaPorCategoria(OpCategorias cat, OpStatus status, boolean statusEqual)
 			throws Exception {
-		entityManager = JpaUtil.getEntityManager();
 		String sql;
 		if (statusEqual) {
 			sql = "SELECT v FROM Veiculo v WHERE v.categoria = :categoria AND v.status = :status";
@@ -65,7 +64,6 @@ public class VeiculoRepository extends AbstractRepository<Veiculo> {
 	}
 
 	public List<Veiculo> buscaPorParametros(OpStatus status, OpMarcas marca) throws Exception {
-		entityManager = JpaUtil.getEntityManager();
 		String sql = "SELECT v FROM Veiculo	v WHERE v.status = :status AND v.marca = :marca";
 		TypedQuery<Veiculo> query = entityManager.createQuery(sql, Veiculo.class);
 		query.setParameter("status", status);
@@ -74,7 +72,6 @@ public class VeiculoRepository extends AbstractRepository<Veiculo> {
 	}
 
 	public List<Veiculo> buscaPorParametros(OpStatus status, OpCategorias cat) throws Exception {
-		entityManager = JpaUtil.getEntityManager();
 		String sql = "SELECT v FROM Veiculo v WHERE v.status = :status AND v.categoria = :categoria";
 		TypedQuery<Veiculo> query = entityManager.createQuery(sql, Veiculo.class);
 		query.setParameter("status", status);
