e06bed7b63c5aeaeca8c603084b25ed4c0045b14
tglima <thg.limadesousa@gmail.com>
2017-12-02 20:50:10 -0200

    Atualiza FuncionarioRepository para suportar o EntityManager injetado

--- src/main/java/br/edu/tglima/locadora/repository/FuncionarioRepository.java
+++ src/main/java/br/edu/tglima/locadora/repository/FuncionarioRepository.java
@@ -2,20 +2,22 @@ package br.edu.tglima.locadora.repository;
 
 import java.util.List;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
 import br.edu.tglima.locadora.models.pessoa.Funcionario;
-import br.edu.tglima.locadora.util.JpaUtil;
 
 public class FuncionarioRepository extends AbstractRepository<Funcionario> {
 
 	public FuncionarioRepository() {
 		super(Funcionario.class);
-		entityManager = JpaUtil.getEntityManager();
 	}
 
+	@Inject
+	private EntityManager entityManager;
+
 	public Funcionario buscarPorCPF(String cpf) throws Exception {
-		entityManager = JpaUtil.getEntityManager();
 		String sql = "SELECT f FROM Funcionario f WHERE f.cpf = :cpf";
 		TypedQuery<Funcionario> query = entityManager.createQuery(sql, Funcionario.class);
 		query.setParameter("cpf", cpf);
@@ -23,7 +25,6 @@ public class FuncionarioRepository extends AbstractRepository<Funcionario> {
 	}
 
 	public List<Funcionario> buscaPorNome(String nome, String sobrenome) throws Exception {
-		entityManager = JpaUtil.getEntityManager();
 		String sql = "SELECT f from Funcionario f WHERE f.nome LIKE :nome OR f.sobrenome LIKE :sobrenome";
 		TypedQuery<Funcionario> query = entityManager.createQuery(sql, Funcionario.class);
 		query.setParameter("nome", "%" + nome.toLowerCase() + "%");
